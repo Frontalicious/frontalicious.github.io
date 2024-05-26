9be1c1f0b9c4035c3521d473577169b42f412dcd
Martijn <martijn@frontalicious.nl>
2024-05-26 14:14:08 +0200

    Add github action to build and deploy to github pages

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,23 +10,8 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix (SPA Mode)</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/guides/spa-mode"
-            rel="noreferrer"
-          >
-            SPA Mode Guide
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <h1>Frontalicious.nl</h1>
+      Come back soon
     </div>
   );
 }
