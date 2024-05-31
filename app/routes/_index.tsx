2551db7317c4da219cdfc466964f23e4ec228c71
Martijn <martijn@frontalicious.nl>
2024-05-31 21:14:32 +0200

    Add Biome, remove eslint

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,17 +1,20 @@
 import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix SPA" },
-    { name: "description", content: "Welcome to Remix (SPA Mode)!" },
-  ];
+	return [
+		{ title: "Frontalicious - Delicious Web Applications" },
+		{
+			name: "description",
+			content: "Front-end Web development, specialized in React development",
+		},
+	];
 };
 
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Frontalicious.nl</h1>
-      Come back soon
-    </div>
-  );
+	return (
+		<div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+			<h1>Frontalicious.nl</h1>
+			Come back soon
+		</div>
+	);
 }
